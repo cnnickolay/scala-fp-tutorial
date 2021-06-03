5a6e5b63421829fb60ac37ef0a96318a2d442526
Nikolai Cherkezishvili <nikolai.cherkezishvili@digitalasset.com>
2021-06-03 22:15:35 +1000

    big cleanup

--- src/main/scala/org/nikosoft/lessons/Lesson2.scala
+++ src/main/scala/org/nikosoft/lessons/Lesson2.scala
@@ -4,16 +4,19 @@ import org.nikosoft.lessons.Lesson1._
 
 object Lesson2 {
 
-  class ScalaMonad[T, F[T1] <: Monad[T1, F]](m: F[T]) {
-    def flatMap[B](f: T => F[B]): F[B] = m.bind(f)(m)
-    def map[B](f: T => B): F[B] = m.fmap(f)(m)
+  class ScalaMonadWrapper[T, F[_] : Monad](m: F[T]) {
+    def flatMap[B](f: T => F[B]): F[B] = implicitly[Monad[F]].bind(f)(m)
+    def map[B](f: T => B): F[B] = implicitly[Monad[F]].fmap(f)(m)
   }
 
-  implicit def scalaifyMonad[T, F[T1] <: Monad[T1, F]](monad: F[T]): ScalaMonad[T, F] = new ScalaMonad[T, F](monad)
+  object ScalaMonadWrapper {
+    implicit def scalaify[A, F[_] : Monad](f: F[A]): ScalaMonadWrapper[A, F] = new ScalaMonadWrapper[A, F](f)
+  }
 
   def main(args: Array[String]): Unit = {
-      val a = Consoliser(1.0).flatMap(stringGeneratorM).flatMap(integiserM).flatMap(bigDecimaliserM).value
-      println(s"Effectful function via flatMap result ${Consoliser(1.0).flatMap(stringGeneratorM).flatMap(integiserM).flatMap(bigDecimaliserM).value}")
+    import ScalaMonadWrapper._
+    val a = Consoliser(1.0).flatMap(stringGeneratorM).flatMap(integiserM).flatMap(bigDecimaliserM).value
+    println(s"Effectful function via flatMap result ${Consoliser(1.0).flatMap(stringGeneratorM).flatMap(integiserM).flatMap(bigDecimaliserM).value}")
   }
 
 }
--- src/main/scala/org/nikosoft/lessons/Lesson3.scala
+++ src/main/scala/org/nikosoft/lessons/Lesson3.scala
@@ -1,30 +1,30 @@
 package org.nikosoft.lessons
 
-import org.nikosoft.lessons.Lesson1._
-
-object Lesson3 {
-
-  // the intention
-  case class Person(name: String, lastName: String, phone: Option[String])
-
-  private val createPerson: String => String => Option[String] => Person = (Person.apply _).curried
-
-  val person: Consoliser[Person] = Consoliser(createPerson) <*> Consoliser("Niko") <*> Consoliser("Che") <*> Consoliser(Option("0928344"))
-
-  // Applicative type and implementation
-  trait Applicative[A, B] {
-    protected def `<*>`[F[T] <: Monad[T, F]](leftM: F[A => B], f: F[A]): F[B] = f.bind(a =>
-      leftM.fmap { fa =>
-        fa(a)
-      }(leftM))(f)
-  }
-
-  implicit class ApplicativeConsolise[A, B](leftM: Consoliser[A => B]) extends Applicative[A, B] {
-    def `<*>`(f: Consoliser[A]): Consoliser[B] = super.`<*>`[Consoliser](leftM, f)
-  }
-
-  def main(args: Array[String]): Unit = {
-    // run
-    println(person.value)
-  }
-}
+//import org.nikosoft.lessons.Lesson1._
+//
+//object Lesson3 {
+//
+//  // the intention
+//  case class Person(name: String, lastName: String, phone: Option[String])
+//
+//  private val createPerson: String => String => Option[String] => Person = (Person.apply _).curried
+//
+//  val person: Consoliser[Person] = Consoliser(createPerson) <*> Consoliser("Niko") <*> Consoliser("Che") <*> Consoliser(Option("0928344"))
+//
+//  // Applicative type and implementation
+//  trait Applicative[A, B] {
+//    protected def `<*>`[F[T] <: Monad[T, F]](leftM: F[A => B], f: F[A]): F[B] = f.bind(a =>
+//      leftM.fmap { fa =>
+//        fa(a)
+//      }(leftM))(f)
+//  }
+//
+//  implicit class ApplicativeConsolise[A, B](leftM: Consoliser[A => B]) extends Applicative[A, B] {
+//    def `<*>`(f: Consoliser[A]): Consoliser[B] = super.`<*>`[Consoliser](leftM, f)
+//  }
+//
+//  def main(args: Array[String]): Unit = {
+//    // run
+//    println(person.value)
+//  }
+//}
