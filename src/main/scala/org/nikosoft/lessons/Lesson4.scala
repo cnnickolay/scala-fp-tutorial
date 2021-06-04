0ca3a6d6a0aa33f896fd8f03af92239960f3b9b3
Nikolai Cherkezishvili <nikolai.cherkezishvili@digitalasset.com>
2021-06-04 19:12:23 +1000

    applicative made right

--- src/main/scala/org/nikosoft/lessons/Lesson4.scala
+++ src/main/scala/org/nikosoft/lessons/Lesson4.scala
@@ -1,30 +1,37 @@
 package org.nikosoft.lessons
 
-//import org.nikosoft.lessons.Lesson1._
-//
-//object Lesson4 {
-//
-//  // the intention
-//  case class Person(name: String, lastName: String, phone: Option[String])
-//
-//  private val createPerson: String => String => Option[String] => Person = (Person.apply _).curried
-//
-//  val person: Consoliser[Person] = Consoliser(createPerson) <*> Consoliser("Niko") <*> Consoliser("Che") <*> Consoliser(Option("0928344"))
-//
-//  // Applicative type and implementation
-//  trait Applicative[A, B] {
-//    protected def `<*>`[F[T] <: Monad[T, F]](leftM: F[A => B], f: F[A]): F[B] = f.bind(a =>
-//      leftM.fmap { fa =>
-//        fa(a)
-//      }(leftM))(f)
-//  }
-//
-//  implicit class ApplicativeConsolise[A, B](leftM: Consoliser[A => B]) extends Applicative[A, B] {
-//    def `<*>`(f: Consoliser[A]): Consoliser[B] = super.`<*>`[Consoliser](leftM, f)
-//  }
-//
-//  def main(args: Array[String]): Unit = {
-//    // run
-//    println(person.value)
-//  }
-//}
+import org.nikosoft.lessons.Lesson1._
+import org.nikosoft.lessons.Lesson3.Or
+import org.nikosoft.lessons.Lesson3.Or.{good, _}
+
+
+object Lesson4 {
+
+  // the intention
+  case class Person(name: String, lastName: String, phone: Option[String])
+
+  trait Pure[F[_]] {
+    def pure[T]: T => F[T]
+  }
+
+  // Applicative type and implementation
+  trait Applicative[F[_]] extends Monad[F] with Pure[F] {
+    def `<*>`[A, B](f: F[A => B], fa: F[A]): F[B] = bind[A, B] { t => bind[A => B, B]{ m => pure(m(t)) }(f) }(fa)
+  }
+
+  implicit class ApplicativeRich[A, B, F[_] : Applicative](leftM: F[A => B]) {
+    def `<*>`(f: F[A]): F[B] = implicitly[Applicative[F]].`<*>`(leftM, f)
+  }
+
+  implicit val orApplicative: Applicative[Or] = new Applicative[Or] {
+    override def bind[A, B]: (A => Or[B]) => Or[A] => Or[B] = orM.bind
+    override def fmap[A, B]: (A => B) => Or[A] => Or[B] = orM.fmap
+    override def pure[T]: T => Or[T] = good
+  }
+
+  def main(args: Array[String]): Unit = {
+    val createPerson: String => String => Option[String] => Person = (Person.apply _).curried
+    val person: Or[Person] = good(createPerson) <*> ugly(throw new RuntimeException("sorry1")) <*> ugly(throw new RuntimeException("sorry")) <*> good(Option("0928344"))
+    println(person)
+  }
+}
